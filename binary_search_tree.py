e2f8b871433d059b5174b9ca0132d547ec67983e
Ian Baldo <ian.baldo@am4.com.br>
2017-09-28 10:39:28 -0300

    Solved with a strategy suggested by a friend.

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -7,27 +7,24 @@ class node:
         self.left = None
         self.right = None
 """
-# Checks if Left tree if a BST
-def deepL(node, val):
-    if node == None:
-        return 1 # true
+treeValues = []
 
-    if (node.left and (node.left.data >= node.data )) or (node.right and (node.right.data <= node.data or node.right.data >= val)):
-        return 0 # false
+def runTree(root):
+    if root == None:
+        return
     
-    return deepL(node.left, node.data) and deepR(node.right, val)
+    runTree(root.left)
+    treeValues.append(root.data)
+    runTree(root.right)
 
-# Checks if Right tree is BST
-def deepR(node, val):
-    if node == None:
-        return 1 # true
-    
-    if (node.left and (node.left.data >= node.data or node.left.data <= val )) or (node.right and (node.right.data <= node.data)):
-        return 0 # false
-    
-    return deepL(node.left, val) and deepR(node.right, node.data)
 
 def checkBST(root):
     if root == None:
         return 1 # true
-    return deepL(root.left,root.data) and deepR(root.right,root.data)
+
+    runTree(root)
+    for i in range(len(treeValues)):
+        for j in range(i+1,len(treeValues)):
+            if treeValues[i] >= treeValues[j]:
+                return 0
+    return 1
